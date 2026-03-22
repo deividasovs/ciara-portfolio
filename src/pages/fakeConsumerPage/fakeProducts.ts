03b5c61f13253d2e2f31cd12d517261839198554
Deividas Ovsianikovas <deividasovs@gmail.com>
2026-03-22 13:29:50 +0000

    fix path

--- src/pages/fakeConsumerPage/fakeProducts.ts
+++ src/pages/fakeConsumerPage/fakeProducts.ts
@@ -7,10 +7,10 @@ export interface Product {
 }
 
 export const fakeProducts: Product[] = [
-    { id: 1, name: "Prince's Ego Tee", originalPrice: 60.00, discountedPrice: 0.99, image: "" },
-    { id: 2, name: "Barcode Socks", originalPrice: 20.00, discountedPrice: 0.49, image: "" },
-    { id: 3, name: "Paperclip Skirt", originalPrice: 120.00, discountedPrice: 1.99, image: "" },
-    { id: 4, name: "Lingerie Set", originalPrice: 40.00, discountedPrice: 0.79, image: "" },
-    { id: 5, name: "Paperbag Long Tee", originalPrice: 70.00, discountedPrice: 1.49, image: "" },
-    { id: 6, name: "Paperbag Clutch", originalPrice: 130.00, discountedPrice: 0.69, image: "" },
+    { id: 1, name: "Prince's Ego Tee", originalPrice: 60.00, discountedPrice: 0.99, image: '/assets/images/fakeConsumerPage/papershirt.png' },
+    { id: 2, name: "Barcode Socks", originalPrice: 20.00, discountedPrice: 0.49, image: '🧦' },
+    { id: 3, name: "Paperclip Skirt", originalPrice: 120.00, discountedPrice: 1.99, image: '📎' },
+    { id: 4, name: "Lingerie Set", originalPrice: 40.00, discountedPrice: 0.79, image: '/assets/images/fakeConsumerPage/limitededitionbra.jpg' },
+    { id: 5, name: "Paperbag Long Tee", originalPrice: 70.00, discountedPrice: 1.49, image: '/assets/images/fakeConsumerPage/paperbagtee.png' },
+    { id: 6, name: "Paperbag Clutch", originalPrice: 130.00, discountedPrice: 0.69, image: '/assets/images/fakeConsumerPage/paperbagclutch.png' },
 ];
